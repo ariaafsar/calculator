a30c227ee400a3d8823bccf442065a939fda484f
aria_afsar <aria.legion2@gmail.com>
2024-08-13 04:22:27 +0330

    fixed input code

--- cal.py
+++ cal.py
@@ -18,16 +18,16 @@ def main():
     while True :
         inp = input("choose your action please :\n1.sum 2.subtraction 3.multipication 4.division 5.exit\n")
         if inp == "1" or inp == "sum" :
-            num1 , num2 = int(input("enter your numbers please : <number 1> <number 2>").split())
+            num1 , num2 = map(int , input("enter your numbers please : <number 1> <number 2>").split())
             sum(num1 , num2)
         elif inp == "2" or inp == "subtraction" :
-            num1 , num2 = int(input("enter your numbers please : <number 1> <number 2>").split())
+            num1 , num2 = map(int , input("enter your numbers please : <number 1> <number 2>").split())
             sub(num1 , num2)
         elif inp == "3" or inp == "multipication" :
-            num1 , num2 = int(input("enter your numbers please : <number 1> <number 2>").split())
+            num1 , num2 = map(int , input("enter your numbers please : <number 1> <number 2>").split())
             multi(num1 , num2)
         elif inp == "4" or inp == "division" :
-            num1 , num2 = int(input("enter your numbers please : <number 1> <number 2>").split())
+            num1 , num2 = map(int , input("enter your numbers please : <number 1> <number 2>").split())
             div(num1 , num2)
         elif inp == "5" or inp == "exit" :
             break
